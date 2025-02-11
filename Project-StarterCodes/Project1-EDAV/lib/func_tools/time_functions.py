245b787530026e5d08ca842ee3ecc8007ad95480
andrewfagerheim <amf2288@columbia.edu>
2025-02-11 01:13:47 +0000

    new file for time selection functions

---
+++ Project-StarterCodes/Project1-EDAV/lib/func_tools/time_functions.py
@@ -0,0 +1,48 @@
+### time selection functions
+
+#imports
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+def to_datetime64(ds):
+    """
+    Takes a ds with time as a string and returns a ds with coordinate iso_time
+    that has type datetime64
+    """
+    
+    iso_time_flat = ds['iso_time'].values.flatten()
+    iso_time_flat_str = iso_time_flat.astype(str)
+    iso_time_flat_dt = pd.to_datetime(iso_time_flat_str, errors='coerce').to_numpy().reshape(ds['iso_time'].shape)
+
+    ds['iso_time'] = (('storm', 'date_time'), iso_time_flat_dt)
+    ds = ds.set_coords('iso_time')
+
+    return ds
+
+def add_origin_year(ds):
+    """
+    Takes a ds with coordinate of time and selects the first year in the time
+    array. This new coordinate is called origin_year
+    """
+    
+    origin_years = []
+    for storm in range(0,len(ds.storm)):
+        origin_year = int(ds.iso_time.dt.year[storm][0])
+        origin_years.append(origin_year)
+
+    ds['origin_year'] = ('storm', origin_years)
+    ds = tks.set_coords('origin_year')
+
+    return ds
+
+def sel_years(ds, start_year, end_year):
+    """
+    Takes a ds and selects only the data between the start_year and end_year inputs
+    based on the origin_year coordinate.
+    """
+    
+    mask = (tks.origin_year >= start_year) & (tks.origin_year <= end_year)
+    tks_filtered = tks.where(mask, drop=True)
+
+    return tks_filtered
